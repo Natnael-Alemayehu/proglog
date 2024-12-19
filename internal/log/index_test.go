500865429c642b00a6192d7bdce165dba13beca2
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-12-19 13:32:45 +0300

    Segment test implemented. Works fine.

--- internal/log/index_test.go
+++ internal/log/index_test.go
@@ -18,7 +18,7 @@ func TestIndex(t *testing.T) {
 	idx, err := newIndex(f, c)
 	require.NoError(t, err)
 	_, _, err = idx.Read(-1)
-	require.NoError(t, err)
+	require.Equal(t, io.EOF, err)
 	require.Equal(t, f.Name(), idx.Name())
 
 	entries := []struct {
