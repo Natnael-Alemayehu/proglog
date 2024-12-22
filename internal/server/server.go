10eb15ff66b5642fe57467fabe98edb7ce653dd1
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-12-22 13:32:57 +0300

    grpc from api/v1 generated

---
+++ internal/server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/natnael-alemayehu/proglog/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
