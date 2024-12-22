45a594aab2c2af5145902cebe91491f644e75a49
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-12-22 14:57:17 +0300

    grpc server completed

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	api "github.com/natnael-alemayehu/proglog/api/v1"
+	"google.golang.org/grpc"
 )
 
 type Config struct {
@@ -11,6 +12,16 @@ type Config struct {
 }
 
 var _ api.LogServer = (*grpcServer)(nil)
+
+func NewGRPCServer(cofig *Config) (*grpc.Server, error) {
+	gsrv := grpc.NewServer()
+	srv, err := newgrpcServer(cofig)
+	if err != nil {
+		return nil, err
+	}
+	api.RegisterLogServer(gsrv, srv)
+	return gsrv, nil
+}
 
 type grpcServer struct {
 	api.UnimplementedLogServer
@@ -30,6 +41,52 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api
 		return nil, err
 	}
 	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(uint64(req.Offset))
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
 }
 
 type CommitLog interface {
